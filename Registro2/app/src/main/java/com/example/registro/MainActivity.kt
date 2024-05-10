ef0ce6f27388fdab6b738460eea223f39bc5bb9b
Sofia <daniela.as.alejandra37@gmail.com>
2024-05-10 16:25:48 -0500

    cositas

---
+++ Registro2/app/src/main/java/com/example/registro/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.registro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
